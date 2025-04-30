2cb25926834b52adb74970f7ab2800bbfa618c28
stenchh <eternityflicker@gmail.com>
2025-04-30 16:41:46 +0300

    first commit

--- tests/test_set_name_and_price_for_a_bun.py
+++ tests/test_set_name_and_price_for_a_bun.py
@@ -2,13 +2,13 @@ from praktikum.bun import Bun
 import pytest
 
 class TestBunModel:
-    def test_set_valid_and_price_name_for_bun(self):
+    def test_bun_has_valid_name(self):
         bun = Bun(name="Ordinary Bun", price=0.5)
-        assert bun.get_name() == "Ordinary Bun"
-        assert bun.get_price() == 0.5
+        assert bun.name == "Ordinary Bun"
 
-
-
+    def test_bun_has_valid_price(self):
+        bun = Bun(name="Ordinary Bun", price=0.5)
+        assert bun.price == 0.5
 
 
 
